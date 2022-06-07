eaa4fa45fe451706dc3db567a2a8bab0fada7244
fcraighero <97830527+fcraighero@users.noreply.github.com>
2022-06-08 00:05:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
    name='eff-densenet',
    version="0.1.0",
-   package_dir={'':'models'},
+   package_dir={'model':'models'},
    packages=find_packages(where="./models"),
 )
