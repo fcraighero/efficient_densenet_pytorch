ee1eb41aa260d7cb2c80d8fdbe9d53c4a5e8fb33
fcraighero <97830527+fcraighero@users.noreply.github.com>
2022-06-08 13:25:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
    name='effdensenet',
    version="0.1.0",
-   package_dir={'effdensenet': 'models'}
+   package_dir={'effdensenet': 'models'},
    packages=['effdensenet']
    
 )
